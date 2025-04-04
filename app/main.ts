d67c9db12fa6c97604ace035d60039a4db390353
venkateshpandirnki <venkateshpandrinki3@gmail.com>
2025-04-04 15:38:42 +0530

    respond with body

--- app/main.ts
+++ app/main.ts
@@ -3,21 +3,27 @@ import * as net from "net";
 const server = net.createServer((socket) => {
   // socket.write(Buffer.from(`HTTP/1.1 200 OK\r\n\r\n`));
   // socket.end();
-  socket.on("data",(data) =>{
+  socket.on("data", (data) => {
     const request = data.toString();
     const requestLine = request.split("\r\n")[0];
-    const [method,path] = requestLine.split(" ");
-    
+    const [method, path] = requestLine.split(" ");
+
     let response;
-    if (path === "/") {
+    if (path.startsWith("/echo/")) {
+      const echoString = path.slice(6);
+      const contentLength = Buffer.byteLength(echoString);
+
+      response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${contentLength}\r\n\r\n${echoString}`;
+    } else if (path === "/") {
       response = "HTTP/1.1 200 OK\r\n\r\n";
     } else {
       response = "HTTP/1.1 404 Not Found\r\n\r\n";
     }
-    
-    socket.write(Buffer.from(response));
-    socket.end()
-  })
+
+    socket.write(response, () => {
+      socket.end();
+    });
+  });
 
   socket.on("close", () => {
     socket.end();
