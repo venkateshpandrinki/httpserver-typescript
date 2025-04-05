6f61a80a87111a4a6adaf548c89e7c4f38129f44
venkateshpandirnki <venkateshpandrinki3@gmail.com>
2025-04-05 15:28:54 +0530

    final

--- app/main.ts
+++ app/main.ts
@@ -1,8 +1,8 @@
 import * as net from "net";
 import * as fs from "fs";
 import * as path from "path";
+import * as zlib from "zlib";
 
-// Read directory from command-line flag
 const dirIndex = process.argv.indexOf("--directory");
 const directory = dirIndex !== -1 ? process.argv[dirIndex + 1] : null;
 
@@ -24,27 +24,27 @@ const server = net.createServer((socket) => {
     const body = request.slice(bodyIndex + 4);
 
     let response = "";
+    let sendBuffer: Buffer | null = null;
 
     if (reqPath === "/") {
       response = `HTTP/1.1 200 OK\r\n\r\n`;
 
     } else if (reqPath.startsWith("/echo/")) {
       const echoString = reqPath.slice(6);
-      let body = echoString;
-      const contentLength = Buffer.byteLength(body);
-
       const acceptEncoding = headers["accept-encoding"];
       const shouldCompress = acceptEncoding?.includes("gzip");
-      const contentEncodingHeader = shouldCompress
-        ? "Content-Encoding: gzip\r\n"
-        : "";
 
-      response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n${contentEncodingHeader}Content-Length: ${contentLength}\r\n\r\n${body}`;
+      if (shouldCompress) {
+        const compressed = zlib.gzipSync(echoString);
+        response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Encoding: gzip\r\nContent-Length: ${compressed.length}\r\n\r\n`;
+        sendBuffer = compressed;
+      } else {
+        response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${Buffer.byteLength(echoString)}\r\n\r\n${echoString}`;
+      }
 
     } else if (reqPath === "/user-agent") {
       const userAgent = headers["user-agent"] || "";
-      const contentLength = Buffer.byteLength(userAgent);
-      response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${contentLength}\r\n\r\n${userAgent}`;
+      response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${Buffer.byteLength(userAgent)}\r\n\r\n${userAgent}`;
 
     } else if (reqPath.startsWith("/files/") && directory) {
       const filename = reqPath.slice(7);
@@ -61,9 +61,7 @@ const server = net.createServer((socket) => {
         } else {
           response = `HTTP/1.1 404 Not Found\r\n\r\n`;
         }
-      }
-
-      else if (method === "POST") {
+      } else if (method === "POST") {
         fs.writeFileSync(filepath, body);
         response = `HTTP/1.1 201 Created\r\n\r\n`;
       }
@@ -72,7 +70,12 @@ const server = net.createServer((socket) => {
       response = `HTTP/1.1 404 Not Found\r\n\r\n`;
     }
 
-    socket.write(response, () => socket.end());
+    socket.write(response, () => {
+      if (sendBuffer) {
+        socket.write(sendBuffer);
+      }
+      socket.end();
+    });
   });
 
   socket.on("close", () => socket.end());
